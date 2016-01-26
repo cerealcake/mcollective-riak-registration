c84b5777431df8158b676b98bfe20b9b2075b905
Jaime Viloria <jaime.viloria@gmail.com>
2016-01-26 17:46:21 +0100

    Add simple agent to send mcollective node registration data to a riak node

---
+++ README.md
@@ -0,0 +1,2 @@
+Riak Discovery Plugin for Mcollective
+
---
+++ agent/registration.rb
@@ -0,0 +1,50 @@
+module MCollective
+  module Agent
+    class Registration<RPC::Agent
+      attr_reader :timeout, :meta
+
+      def initialize
+        @meta = {:license => "Apache 2",
+          :author => "Jaime Viloria <jaime.viloria@gmail.com>",
+          :url => "https://github.com/cerealcake/mcollective-riak-registration"}
+
+        require 'riak'
+
+        @config = Config.instance
+
+        @riak_node = @config.pluginconf["registration.riak_node"] || "localhost"
+        @riak_bucket = @config.pluginconf["registration.riak_bucket"] || "mco"
+
+        Log.instance.debug("Connecting to #{@riak_node} with bucket #{@riak_bucket}")
+        @client = Riak::Client.new(:host => "#{@riak_node}")
+        @bucket = @client.bucket("#{@riak_bucket}")
+
+      end
+
+      def handlemsg(msg, connection)
+        data = msg[:body]
+
+        if (msg.kind_of?(Array))
+          Log.instance.warn("Got no facts - did you forget to add 'registration = Meta' to your server.cfg?");
+          return nill
+        end
+
+        data[:lastseen] = Time.now
+
+        object = @bucket.get_or_new("#{data[:identity]}")
+        object.data = data
+        object.store
+
+        Log.instance.warn("node #{data[:identity]} stored in #{@riak_bucket} on riak node #{@riak_node}");
+
+        nil
+      end
+
+      def help
+      end
+    end
+  end
+end
+
+# vi:tabstop=2:expandtab:ai:filetype=ruby
+
