a9d52fddf79e74e39d4f824ec9d187b75acc8320
Jaime Viloria <jaime.viloria@gmail.com>
2016-01-28 17:28:08 +0100

    added index and bucket_types

--- agent/registration.rb
+++ agent/registration.rb
@@ -13,27 +13,31 @@ module MCollective
         @config = Config.instance
 
         @riak_node = @config.pluginconf["registration.riak_node"] || "localhost"
-        @riak_bucket = @config.pluginconf["registration.riak_bucket"] || "mco"
+        @riak_bucket_type = @config.pluginconf["registration.riak_bucket_type"] || "mcollective"
+        @riak_bucket = @config.pluginconf["registration.riak_bucket"] || "registration"
 
         Log.instance.debug("Connecting to #{@riak_node} with bucket #{@riak_bucket}")
+
         @client = Riak::Client.new(:host => "#{@riak_node}")
-        @bucket = @client.bucket("#{@riak_bucket}")
+        @client.create_search_index("node")
+
+        @bucket_type = @client.bucket_type "#{@riak_bucket_type}"
+        @bucket = @bucket_type.bucket "#{@riak_bucket}"
+
+        @client.set_bucket_props @bucket, {search_index: "node", dvv_enabled: false,last_write_wins: true}, "#{@riak_bucket_type}"
 
       end
 
       def handlemsg(msg, connection)
-        data = msg[:body]
+        data = msg[:body].to_h
 
         if (msg.kind_of?(Array))
           Log.instance.warn("Got no facts - did you forget to add 'registration = Meta' to your server.cfg?");
-          return nill
+          return nil
         end
-
-        data[:lastseen] = Time.now
-
         object = @bucket.get_or_new("#{data[:identity]}")
         object.data = data
-        object.store
+        object.store type: "#{@riak_bucket_type}"
 
         Log.instance.debug("node #{data[:identity]} stored in #{@riak_bucket} on riak node #{@riak_node}");
 
