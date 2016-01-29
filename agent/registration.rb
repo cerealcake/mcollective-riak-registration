c4a515c6b009d8616aefd92fdf5b5cdb27d463dc
Jaime Viloria <jaime.viloria@gmail.com>
2016-01-29 17:05:52 +0100

    added certificate based authentication

--- agent/registration.rb
+++ agent/registration.rb
@@ -12,32 +12,65 @@ module MCollective
 
         @config = Config.instance
 
-        @riak_node = @config.pluginconf["registration.riak_node"] || "localhost"
-        @riak_bucket_type = @config.pluginconf["registration.riak_bucket_type"] || "mcollective"
-        @riak_bucket = @config.pluginconf["registration.riak_bucket"] || "registration"
+        @node = @config.pluginconf["registration.riak_node"] || "localhost"
+        @bucket_type = @config.pluginconf["registration.riak_bucket_type"] || "mcollective"
+        @bucket = @config.pluginconf["registration.riak_bucket"] || "node"
+        @port = @config.pluginconf["registration.riak_port"] || "8087"
 
-        Log.instance.debug("Connecting to #{@riak_node} with bucket #{@riak_bucket}")
+        @user = @config.pluginconf["registration.riak_user"] || "zedo"
+        @password = @config.pluginconf["registration.riak_password"] || 'catnip'
 
-        @client = Riak::Client.new(:host => "#{@riak_node}")
-        @client.create_search_index("node")
+        # The default values for the certificates are useful if your nodes are provisioned via Puppet
+        # On the Riak cluster nodes, the following ca files would correspond to
+        # ssl.certfile = /var/lib/puppet/ssl/certs/fqdn.pem
+        # ssl.keyfile = /var/lib/puppet/ssl/private_keys/fqdn.pem
+        # ssl.cacertfile = /var/lib/puppet/ssl/certs/ca.pem
+        @ca_file = @config.pluginconf["registration.riak_ca_file"] || '/var/lib/puppet/ssl/certs/ca.pem'
+        @client_ca = @config.pluginconf["registration.riak_client_ca"] ||  '/var/lib/puppet/ssl/certs/ca.pem'
+        @cert = @config.pluginconf["registration.riak_cert"] || '/var/lib/puppet/ssl/certs/fqdn.pem'
+        @key = @config.pluginconf["registration.riak_key"] || '/var/lib/puppet/ssl/private_keys/fqdn.pem'
 
-        @bucket_type = @client.bucket_type "#{@riak_bucket_type}"
-        @bucket = @bucket_type.bucket "#{@riak_bucket}"
+        Log.instance.debug("Connecting to #{@node} with bucket #{@bucket}")
 
-        @client.set_bucket_props @bucket, {search_index: "node", dvv_enabled: false,last_write_wins: true}, "#{@riak_bucket_type}"
+        @client = Riak::Client.new( 
+          :authentication => {
+             user: @user,
+             password: @password,
+             ca_file: @ca_file,
+             client_ca:  @client_ca,
+             cert: (File.read @cert),
+             key: OpenSSL::PKey::RSA.new(File.read @key),
+          },
+          :host => @node, 
+          :pb_port => @port
+        )
 
+        # requires 'search = on' to be set in riak.conf on the riak cluster nodes in order to work
+        @client.create_search_index("facts")
+
+        # requires bucket_type to be created already in the riak cluster nodes in order to work
+        # e.g. 
+        # riak-admin bucket-type create mcollective  '{"props": {"dvv_enabled": false, "last_write_wins":true}}'
+        # riak-admin bucket-type activate mcollective
+        @bucket_type = @client.bucket_type "#{@bucket_type}"
+        @bucket = @bucket_type.bucket "#{@bucket}"
+
+        @client.set_bucket_props @bucket, {search_index: "facts", dvv_enabled: false,last_write_wins: true}, "#{@bucket_type}"
+
+      rescue Exception => e
+        Log.instance.error("Failed to connect to riak: #{e}")
       end
 
       def handlemsg(msg, connection)
-        data = msg[:body].to_h
-
+        data = msg[:body]
+        
         if (msg.kind_of?(Array))
           Log.instance.warn("Got no facts - did you forget to add 'registration = Meta' to your server.cfg?");
           return nil
         end
         object = @bucket.get_or_new("#{data[:identity]}")
         object.data = data
-        object.store type: "#{@riak_bucket_type}"
+        object.store type: "#{@bucket_type}"
 
         Log.instance.debug("node #{data[:identity]} stored in #{@riak_bucket} on riak node #{@riak_node}");
 
